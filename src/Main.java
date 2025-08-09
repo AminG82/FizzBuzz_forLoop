b5deaf70e7107283f5a3584d6eb982cab61c108d
AminG82 <mmnghasemi@gmail.com>
2025-08-09 13:16:48 +0330

    fizzBuzz method logic is now more readable and is used in a for loop // this loop will from zero to user input and shows every number , their result

--- src/Main.java
+++ src/Main.java
@@ -3,37 +3,31 @@ import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
 
-    System.out.print("Enter your digit : ");
-    String input = ConsoleReadLine();
+        System.out.print("Enter your digit : ");
+        String input = consoleReadLine();
 
-    String outPut = fizzBuzz(Integer.parseInt(input));
+        for (int i = 0; i <= Integer.parseInt(input); i++) {
 
-    System.out.println(outPut);
-    }
+            String outPut = fizzBuzz(i);
 
-    public static String fizzBuzz(int input){
-
-        String result;
-
-        if (input % 3 == 0 && input % 5 == 0){
-           result = "FizzBuzz";
-        } else if (input % 3 == 0) {
-            result = "Fizz";
-        } else if (input % 5 == 0) {
-            result = "Buzz";
-        }else {
-            result = String.format("Number is : %s" , input);
+            System.out.println(outPut);
         }
-        return result;
     }
 
-    public static String ConsoleReadLine(){
-        Scanner scanner = new Scanner(System.in);
-        String result = scanner.nextLine();
-        scanner.close();
-        return result;
+    public static String fizzBuzz(int input) {
+
+        if (input % 15 == 0) return "FizzBuzz";
+        if (input % 3 == 0) return "Fizz";
+        if (input % 5 == 0) return "Buzz";
+        return String.format("Number is %d", input);
+    }
+
+    public static String consoleReadLine() {
+        return scanner.nextLine();
     }
 }
 
