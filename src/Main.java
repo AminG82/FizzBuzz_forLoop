e2073b4ffd61777847f6cbe29741203bd8ee5765
AminG82 <mmnghasemi@gmail.com>
2025-08-09 12:20:59 +0330

    Fizz an buzz created and works fine but it uses user input and works with if statements

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,37 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
+    System.out.print("Enter your digit : ");
+    String input = ConsoleReadLine();
+
+    String outPut = fizzBuzz(Integer.parseInt(input));
+
+    System.out.println(outPut);
+    }
+
+    public static String fizzBuzz(int input){
+
+        String result;
+
+        if (input % 3 == 0 && input % 5 == 0){
+           result = "FizzBuzz";
+        } else if (input % 3 == 0) {
+            result = "Fizz";
+        } else if (input % 5 == 0) {
+            result = "Buzz";
+        }else {
+            result = String.format("Number is : %s" , input);
+        }
+        return result;
+    }
+
+    public static String ConsoleReadLine(){
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
     }
 }
 
@@ -11,4 +40,4 @@ public class Main {
 //        2. If `n` is divisible by 3 → print `"Fizz"`
 //        3. If `n` is divisible by 5 → print `"Buzz"`
 //        4. If divisible by both → print `"FizzBuzz"`
-//        5. Otherwise → print `"Number: n"`
+//        5. Otherwise, → print `"Number: n"`
