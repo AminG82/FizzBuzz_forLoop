73eab66df18a5f309a5738fd51f0b56c33d0c757
AminG82 <mmnghasemi@gmail.com>
2025-08-09 11:38:47 +0330

    initialCommit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+
+//        1. Takes an integer `n`
+//        2. If `n` is divisible by 3 → print `"Fizz"`
+//        3. If `n` is divisible by 5 → print `"Buzz"`
+//        4. If divisible by both → print `"FizzBuzz"`
+//        5. Otherwise → print `"Number: n"`
